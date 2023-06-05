47556353df386deb0a530778828b7bd9145bb8f2
Jkm036 <joshkmuthii@gmail.com>
2023-06-05 08:22:33 -0400

    Basic Nav Bar Implemented

--- Hermit/src/app/navbar/navbar.component.ts
+++ Hermit/src/app/navbar/navbar.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  host:{
+    "(window:click)" :  "closeSubMenu()"
+  }
 })
 export class NavbarComponent {
-
+ menuOpen:boolean=false;
+  
+  closeSubMenu(){
+    this.menuOpen=false;
+  }
+ toggleSubMenu($event:any){
+  $event.stopPropagation();
+  this.menuOpen=!this.menuOpen;
+  console.log(this.menuOpen);
+ }
 }
