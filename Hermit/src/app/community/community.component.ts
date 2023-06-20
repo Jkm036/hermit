ce2076ec76b8b65b76d8812f30b31b9ed19c7ab8
Andy Zhao <andyzhao21@gmail.com>
2023-06-20 14:05:39 -0400

    Each community will have their respective names displayed

--- Hermit/src/app/community/community.component.ts
+++ Hermit/src/app/community/community.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-community',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class CommunityComponent {
 
+  @Input() name: string = ''
+
+  /**
+   * This constructuor will retrieve the parameters that are being passed to it. 
+   * 
+   * @param router The router link to the component
+   */
+  constructor(private router: ActivatedRoute) {
+    this.name = this.router.snapshot.paramMap.get('name');
+  }
+
+  /**
+   * This will update the name parameter whenever the name 
+   * changes. 
+   * 
+   * i.e. when the user clicks on another community, it will update the name. 
+   * 
+   * Without this, the community will not be updated to the specified community. 
+   */
+  ngOnInit(): void {
+    this.router.params.subscribe(
+      params => {
+        const id = params['name'];
+        this.name = id
+      }
+    );
+  }
 }
--- Hermit/src/app/sidemenu/sidemenu.component.ts
+++ Hermit/src/app/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
-import { Component ,Input,Output,EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'sidemenu',
@@ -6,14 +7,15 @@ import { Component ,Input,Output,EventEmitter} from '@angular/core';
   styleUrls: ['./sidemenu.component.css']
 })
 export class SidemenuComponent {
-  @Input('sideMenuOpen') sideMenuOpen:boolean=true;
-  @Output('toggleHome') toggleH? = new EventEmitter
-  toggleSideMenu(){
-    this.sideMenuOpen=!this.sideMenuOpen;
-   }
-   
-  toggleHEvent(){
+  @Input('sideMenuOpen') sideMenuOpen: boolean = true;
+  @Output('toggleHome') toggleH?= new EventEmitter
+  @Input() name: string = ''
+  toggleSideMenu() {
+    this.sideMenuOpen = !this.sideMenuOpen;
+  }
+
+  toggleHEvent() {
     this.toggleH.emit();
 
-   }
+  }
 }
